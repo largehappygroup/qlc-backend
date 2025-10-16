4f3e7e5aa57f9d832a8be7e7e740d5a8ebb35b04
draw-n <helen.wu1219@gmail.com>
2025-10-16 11:36:33 -0500

    figured out why distributions not showing up

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const connectDB = require("./config/database.js");
 
 const port = process.env.PORT || 5704;
-/*
+
 const allowedOrigin = process.env.FRONTEND_URL || "http://localhost:5703";
 
 const corsOptions = {
@@ -12,10 +12,10 @@ const corsOptions = {
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,
     optionsSuccessStatus: 200,
-};*/
+};
 const app = express();
 
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 
 connectDB();
@@ -26,11 +26,11 @@ app.use("//chapters", require("./routes/chapters.router.js"));
 app.use("//exercises", require("./routes/exercises.router.js"));
 app.use("//users", require("./routes/users.router.js"));
 app.get("/", (req, res) => {
-    const remoteUser = req.headers["remote-user"];
-    const givenName = req.headers["remote-user-given-name"];
-    const familyName = req.headers["remote-user-family-name"];
-    const vunetid = req.headers["remote-user-vunetid"];
-    res.send(`Hello, ${givenName} ${familyName} (${vunetid})! ${JSON.stringify(req.headers)}Your remote user is ${remoteUser}.`);
+    // const remoteUser = req.headers["remote-user"];
+    //const givenName = req.headers["remote-user-given-name"];
+    //const familyName = req.headers["remote-user-family-name"];
+    //const vunetid = req.headers["remote-user-vunetid"];
+    res.send(`Hello :D`);
 });
 
 app.listen(port, () => {
