7d2a479c4b75e3712d4ee033df58021eaa783bd2
draw-n <helen.wu1219@gmail.com>
2025-11-18 09:51:00 -0600

    remove queuescheduler altogether

--- services/queue.js
+++ services/queue.js
@@ -1,15 +1,12 @@
-const { Queue, QueueScheduler } = require("bullmq");
+const { Queue } = require("bullmq");
 const IORedis = require("ioredis");
 
 const connection = new IORedis(process.env.REDIS_URL || "redis://127.0.0.1:6379");
-const queueName = "createExercises";
 
-// Ensure scheduler is running to handle stalled jobs
-const queueScheduler = new QueueScheduler(queueName, { connection });
+const queueName = "createExercises";
 const queue = new Queue(queueName, { connection });
 
 module.exports = {
     queue,
-    queueScheduler,
     connection,
 };
